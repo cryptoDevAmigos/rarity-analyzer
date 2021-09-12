69b958aef2e5f01da60d0b74d06b5e9b0f7c37ee
Rick Love <rick@toldpro.com>
2021-09-12 14:43:42 -0500

    Share code setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {testHello} from '@crypto-dev-amigos/common';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {testHello}
         </p>
         <a
           className="App-link"
---
+++ packages/common/src/common-test.ts
@@ -0,0 +1,1 @@
+export const testHello = 'Hi!';
