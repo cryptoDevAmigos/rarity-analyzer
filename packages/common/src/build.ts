a85d176721beae1f40631dd20bdf36b23f2ccbba
Rick Love <rick@toldpro.com>
2021-09-19 19:13:36 -0500

    Build individual token json files

--- packages/common/src/build.ts
+++ packages/common/src/build.ts
@@ -3,10 +3,17 @@ import { INftMetadata } from "./types";
 import { promises as fs } from 'fs';
 import path from 'path';
 
-export async function generateRarityFiles(dataDirName:string): Promise<void> {
+export async function generateRarityFiles({
+    dataDirName,
+    outDirName,
+}:{
+    dataDirName:string, 
+    outDirName:string,
+}): Promise<void> {
     const files = await fs.readdir(dataDirName, { withFileTypes: true });
     const projectFiles = files.filter(x => x.name.endsWith('.project.json'));
     for(const f of projectFiles){
+        const projectName = f.name.replace('.project.json', '');
         const fullProjectFileName = path.join(dataDirName, f.name);
         const fullNftListFileName = fullProjectFileName.replace('.project.json', '.json');
 
@@ -18,10 +25,20 @@ export async function generateRarityFiles(dataDirName:string): Promise<void> {
 
         const fullRarityFileName = fullProjectFileName.replace('.project.json', '.rarities.json');
         await fs.writeFile(fullRarityFileName, JSON.stringify(rarityResult, null, 2));
+
+        // Break apart the output into individual json files
+        for(const t of rarityResult){
+            const tokenRarityFileName = path.join(outDirName, projectName, `${t.nft.id}.json`);
+            await fs.mkdir(path.dirname(tokenRarityFileName), { recursive: true });
+            await fs.writeFile(tokenRarityFileName, JSON.stringify(t, null, 2));
+        }
     }
 };
 
 const run = async () => {
-    const data = await generateRarityFiles('../../data/');
+    const data = await generateRarityFiles({
+        dataDirName:'../../data/',
+        outDirName:'../../web/data/',
+    });
 };
 run();
