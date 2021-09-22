fb572a262df8c828286c79f83ad8738f3db5cf77
Rick Love <rick@toldpro.com>
2021-09-21 20:06:12 -0500

    FIX Overlapping bar text

--- packages/client/src/components/bar-graph.tsx
+++ packages/client/src/components/bar-graph.tsx
@@ -10,19 +10,19 @@ export const BarGraphCell = ({ ratio, text, textRight }:{ ratio: number, text?: 
                 height: '100%',
                 }}>
             </div>
-            {!!textRight && (
-                <div className='bar-text' style={{
-                    position:'absolute',
-                    right: 4,
-                    zIndex: 9,
-                    height: '100%',
-                    }}>
+            <div style={{position:'relative', zIndex: 10, display:'flex', flexDirection:'row'}}>
+                {/* Used to center main text */}
+                <div className='bar-text' style={{opacity:0}}>
                     {textRight}
                 </div>
-            )}
-            <div className='bar-text' style={{ position:'relative', zIndex: 10}}>
-                {text ?? `${(100 * ratio).toFixed(2)}%`}
+                <div className='bar-text' style={{flex:1}}>
+                    {text ?? `${(100 * ratio).toFixed(2)}%`}
+                </div>
+                <div className='bar-text' style={{}}>
+                    {textRight}
+                </div>
             </div>
+            
         </div>
     );
 }
