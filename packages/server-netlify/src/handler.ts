e38cc1d5318468a063b966f44018c3fab4ba852d
Rick Love <rick@toldpro.com>
2021-09-24 18:19:59 -0500

    Create netlify function discord handler

--- packages/server-netlify/src/handler.ts
+++ packages/server-netlify/src/handler.ts
@@ -1,7 +1,56 @@
-import { handleDiscordCommand } from '@crypto-dev-amigos/common-node';
+import { HandlerEvent, HandlerContext } from "@netlify/functions";
+import { DiscordCommandConfig, DiscordRegisterCommandsConfig, DiscordRequestConfig, DiscordRequestError, handleDiscordCommand, handleDiscordRequest } from '@crypto-dev-amigos/common-node';
 
-const handleDiscordRoute = () => {
-    handleDiscordCommand({command:'help'});
+const config: DiscordRequestConfig & DiscordCommandConfig & DiscordRegisterCommandsConfig = {
+    baseDataUrl: 'https://openrarity.xyz/data/',
+    baseWebUrl: 'https://openrarity.xyz/',
+    applicationId: process.env.DISCORD_APPLICATION_ID as string,
+    discordPublicKey: process.env.DISCORD_PUBLIC_KEY as string,
+    botToken: process.env.DISCORD_BOT_TOKEN as string,
 };
 
-export default handleDiscordRoute;
+export const handleDiscordRoute = async (event:HandlerEvent, context:HandlerContext) => {
+
+    try{
+        const result = await handleDiscordRequest({
+            config, 
+            body: JSON.parse(event.body ?? ''), 
+            rawBody: event.body ?? '', 
+            getHeader: (name) => event.headers[name]
+        });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result),
+        };
+    } catch(err) {
+        try{
+            const error = err as DiscordRequestError;
+            if(error.data){
+                if(error.data.textResponse){
+                    return {
+                        statusCode: error.data.statusCode,
+                        body: error.data.textResponse,
+                    };
+                }
+                if(error.data.jsonResponse){
+                    return {
+                        statusCode: error.data.statusCode,
+                        body: JSON.stringify(error.data.jsonResponse),
+                    };
+                }
+
+                return {
+                    statusCode: error.data.statusCode,
+                    body: 'Error',
+                };
+            }
+        } catch {
+            // Ignore
+        }
+
+        return {
+            statusCode: 500,
+            body: 'Oops! Something broke!',
+        };
+    }
+};
--- packages/server-netlify/src/index.ts
+++ packages/server-netlify/src/index.ts
@@ -1,1 +1,16 @@
-export * from './handler';
+import { Handler } from "@netlify/functions";
+import { handleDiscordRoute } from "./handler";
+
+const handler: Handler = async (event, context) => {
+
+    if(event.path.endsWith('/discord')){
+        return await handleDiscordRoute(event, context);
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "What's up?" }),
+    };
+};
+
+export { handler };
