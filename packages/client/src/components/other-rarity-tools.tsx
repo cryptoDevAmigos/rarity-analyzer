8afba4e83ac83633f2e0801c50e3e94cc2467bcc
Rick Love <rick@toldpro.com>
2021-09-28 11:39:48 -0500

    Add other tools

--- packages/client/src/components/other-rarity-tools.tsx
+++ packages/client/src/components/other-rarity-tools.tsx
@@ -1,11 +1,16 @@
 export const OtherRarityTools = ()=>{
     return (
         <>
-            <div style={{marginTop: 64}}>
+            <div style={{margin: 8, marginTop: 64}}>
                 <div>Our Friends - Also making cool tools!</div>
-                <OtherRarityTool name='Punkscapes Cool Rarity' url='https://punkscape.coolrarity.com/' />
-                <OtherRarityTool name='Punkscapes Rarity' url='https://punkscapes-rarity.vercel.app/' />
-                <OtherRarityTool name='Kinesis Rarity' url='https://kinesis-rarity.vercel.app/' />
+                <OtherRarityTool name='Fukuball - One-day-punk Cool Rarity' url='https://onedaypunk-rarity-tool.herokuapp.com/' />
+                <OtherRarityTool name='Fukuball - Punkscapes Cool Rarity' url='https://punkscape.coolrarity.com/' />
+                <OtherRarityTool name='Nishu - One-day-punk Rarity' url='https://odp-rarity.vercel.app/' />
+                <OtherRarityTool name='OxNight - Punkscapes Rarity' url='https://rarity-punkscape.vercel.app/' />
+                <OtherRarityTool name='Mikk-o - Punkscapes Rarity' url='https://punkscapes-rarity.vercel.app/' />
+                <OtherRarityTool name='Mikk-o - Kinesis Rarity' url='https://kinesis-rarity.vercel.app/' />
+                <OtherRarityTool name='Rarity Tools' url='https://rarity.tools' />
+                <OtherRarityTool name='Rarity Guide' url='https://rarity.guide' />
             </div>
         </>
     );
